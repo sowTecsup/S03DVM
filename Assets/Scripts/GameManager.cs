168424ecfd1db55e20b991ee23304e9a8f9d03c7
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-31 12:14:45 -0500

    zzz

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,42 +3,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Action OnPlayerWin;
-    public Func<int> OnPlayerGetSmt;
-
-    public PlayerController player;
-    void Start()
-    {
-        OnPlayerWin += DestroyAllEnemys;
-        OnPlayerWin += ShowVictoryUI;
-        OnPlayerGetSmt += SimpleReturn;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OnPlayerFinishMatch()
-    {
-        OnPlayerWin?.Invoke();
-        OnPlayerGetSmt?.Invoke();   
-    }
-
-    public void DestroyAllEnemys()
-    {
-        Debug.Log("Destroy All Enemies");
-    }
-
-    public void ShowVictoryUI()
-    {
-        Debug.Log("ShowVictoryUI");
-    }
-
-    public int SimpleReturn()
-    {
-        return 12;
-    }
-    
+   
 }
