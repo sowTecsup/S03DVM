d5774d5d6e4be0bb97055771320bd64491542ddf
sowTecsup <jneyra@tecsup.edu.pe>
2026-03-31 09:14:25 -0500

    FirstUpdate

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputSystem_Actions inputs;
+    private CharacterController controller;
+
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 200f;
+
+
+    [SerializeField]private Vector2 moveInput;
+
+    private void Awake()
+    {
+        inputs = new();
+        controller = GetComponent<CharacterController>();
+    }
+    private void OnEnable()
+    {
+        inputs.Enable();
+
+        inputs.Player.Move.performed += ctx =>  moveInput = ctx.ReadValue<Vector2>();
+        inputs.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+
+        // OnMove();
+        OnSimpleMove();
+    }
+    public void OnMove()
+    {
+        transform.Rotate(Vector3.up * moveInput.x * rotationSpeed * Time.deltaTime);
+        Vector3 moveDir = transform.forward * moveSpeed * moveInput.y * Time.deltaTime;
+        controller.Move(moveDir);
+    }
+    public void OnSimpleMove()
+    {
+        transform.Rotate(Vector3.up * moveInput.x * rotationSpeed * Time.deltaTime);
+        Vector3 moveDir = transform.forward * moveSpeed * moveInput.y ;
+        controller.SimpleMove(moveDir);
+    }
+
+}
